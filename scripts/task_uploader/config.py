a8f859e8f7e86a0dca051fa575fdf1a51e197890
Tomasz Kolek <tomasz-kolek@o2.pl>
2016-11-16 13:57:11 +0100

    Add Task uploader script.

---
+++ scripts/task_uploader/config.py
@@ -0,0 +1,13 @@
+# Config file
+
+# This section has to be changed
+API_KEY = "API_KEY"
+FILES = ["file1.csv", "file2.csv"]
+
+
+# This section has not to be changed
+PUBLISH = True
+URL = 'https://codein.withgoogle.com'
+DEBUG = False
+VERBOSE = True
+DELIMITER = ','  # separator for CSV files
