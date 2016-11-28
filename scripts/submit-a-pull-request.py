68877d4761298342bf568c87ca9c83219a0ff111
Tim Abbott <tabbott@zulipchat.com>
2016-11-27 22:33:57 -0800

    submit-a-pull-request: update description.

--- scripts/submit-a-pull-request.py
+++ scripts/submit-a-pull-request.py
@@ -5,8 +5,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--force', dest='force', action="store_true", default=False)
 args = parser.parse_args()
 
-description = """This task will help you get familiarize with making pull request
-to gci-submissions repositary. Instructions are at 
+description = """This task will teach you how to create a pull request, which is an
+important skill to learn for contributing to an open source project on
+GitHub.  Follow the tutorial at
 https://github.com/zulip/zulip-gci/blob/master/tasks/submit-a-pull-request.md.
 """
 
@@ -15,14 +16,14 @@ upload_task(
     name = 'Submit a Pull Request to gci-submissions repositary',
     description = description,
     status = 1, # 1: draft, 2: published
-    max_instances = 200,
-    mentors = ['niftynei@gmail.com', 'rishig@zulipchat.com', 'tabbott@zulipchat.com'],
+    max_instances = 40,
+    mentors = ['yo@vishnuks.com', 'arpith@feedreader.co', 'rishig@zulipchat.com', 'tabbott@zulipchat.com'],
     tags = [], # free text
     is_beginner = True,
     # 1: Coding, 2: User Interface, 3: Documentation & Training,
     # 4: Quality Assurance, 5: Outreach & Research
     categories = [3],
-    time_to_complete_in_days = 3, # must be between 3 and 7
+    time_to_complete_in_days = 5, # must be between 3 and 7
     # Field currently not accessible via API. gci-support says it is coming soon.
     external_url = "https://github.com/zulip/zulip-gci/blob/master/tasks/submit-a-pull-request.md",
     private_metadata = "submit-a-pull-request",
