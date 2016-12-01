2fb285f60a807cb2a3da83bad5b57d0cf01f3276
trueskawka <malavarena@gmail.com>
2016-12-01 03:00:28 +0100

    tasks: Add translation tasks.
    Add tasks for creating translation style guides.
    Add a tasks for improving existing guides and proposing
    new ones.

---
+++ scripts/translations.py
@@ -0,0 +1,179 @@
+import argparse
+from upload import upload_task
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--force', dest='force', action="store_true", default=False)
+args = parser.parse_args()
+
+languages = [
+    ('Simplified Chinese'),
+    ('German'),
+    ('Russian'),
+    ('Italian'),
+    ('French'),
+    ('Turkish'),
+    ('Portuguese'),
+    ('Arabic'),
+    ('Hindi'),
+    ('Japanese'),
+    ('Malayalam'),
+    ('Serbian'),
+    ('Hungarian'),
+    ('Korean')
+]
+
+# existing guides in need of improvement
+guides = [
+    ('Polish'),
+    ('Spanish')
+]
+
+description = """Zulip is a tool used by many teams daily. In order to make it more approachable
+to users, there is an ongoing translation effort. [Read more about translating
+Zulip](https://zulip.readthedocs.io/en/latest/translating.html).
+
+This group of tasks is about creating common translation style guides, to give
+guidance on how Zulip should be translated into specific languages. Currently
+there are guides for [Polish](https://zulip.readthedocs.io/en/latest/polish.html)
+and [Spanish](https://zulip.readthedocs.io/en/latest/spanish.html).
+
+"""
+
+description_ABCD = description + "For this task, do **Task Type %(type)s** for " + \
+                  "**%(language)s**"
+description_E = description + """If you are a native speaker of a language that
+does not have a style guide yet and there is no translation task for it, feel
+free to propose a language for translation tasks!
+
+Before you claim this task, make sure to ask on Zulip `GCI tasks` stream if an
+appropriate task does not already exist.
+
+Once you get confirmation, claim the task and do **Task Type E**.
+"""
+
+for language in languages:
+    # Task Type A
+    upload_task(
+        # https://developers.google.com/open-source/gci/resources/downloads/TaskAPISpec.pdf
+        name = '%s: Research web application translations' % (language),
+        description = description_ABCD % {'type': 'A', 'language': language},
+        status = 2, # 1: draft, 2: published
+        max_instances = 1,
+        mentors = ['malavarena@gmail.com', 'rishig@zulipchat.com',
+                   'tabbott@zulipchat.com'],
+        tags = ['translation'], # free text
+        is_beginner = False,
+        # 1: Coding, 2: User Interface, 3: Documentation & Training,
+        # 4: Quality Assurance, 5: Outreach & Research
+        categories = [3, 5],
+        time_to_complete_in_days = 5, # must be between 3 and 7
+        # Field currently not accessible via API. gci-support says it is coming soon.
+        # external_url = "https://github.com/zulip/zulip-gci/blob/master/tasks/webhook-integrations.md",
+        private_metadata = "translations-A",
+        do_upload = args.force)
+
+    # Task Type B
+    upload_task(
+        # https://developers.google.com/open-source/gci/resources/downloads/TaskAPISpec.pdf
+        name = '%s: Write general translation rules' % (language),
+        description = description_ABCD % {'type': 'B', 'language': language},
+        status = 2, # 1: draft, 2: published
+        max_instances = 1,
+        mentors = ['malavarena@gmail.com', 'rishig@zulipchat.com',
+                   'tabbott@zulipchat.com'],
+        tags = ['translation'], # free text
+        is_beginner = False,
+        # 1: Coding, 2: User Interface, 3: Documentation & Training,
+        # 4: Quality Assurance, 5: Outreach & Research
+        categories = [3],
+        time_to_complete_in_days = 5, # must be between 3 and 7
+        # Field currently not accessible via API. gci-support says it is coming soon.
+        # external_url = "https://github.com/zulip/zulip-gci/blob/master/tasks/webhook-integrations.md",
+        private_metadata = "translations-B",
+        do_upload = args.force)
+
+    # Task Type C
+    upload_task(
+        # https://developers.google.com/open-source/gci/resources/downloads/TaskAPISpec.pdf
+        name = '%s: Translate special terms used in Zulip' % (language),
+        description = description_ABCD % {'type': 'C', 'language': language},
+        status = 2, # 1: draft, 2: published
+        max_instances = 1,
+        mentors = ['malavarena@gmail.com', 'rishig@zulipchat.com',
+                   'tabbott@zulipchat.com'],
+        tags = ['translation'], # free text
+        is_beginner = False,
+        # 1: Coding, 2: User Interface, 3: Documentation & Training,
+        # 4: Quality Assurance, 5: Outreach & Research
+        categories = [3],
+        time_to_complete_in_days = 5, # must be between 3 and 7
+        # Field currently not accessible via API. gci-support says it is coming soon.
+        # external_url = "https://github.com/zulip/zulip-gci/blob/master/tasks/webhook-integrations.md",
+        private_metadata = "translations-C",
+        do_upload = args.force)
+
+for language in guides:
+    # Task Type A
+    upload_task(
+        # https://developers.google.com/open-source/gci/resources/downloads/TaskAPISpec.pdf
+        name = '%s: Research web application translations' % (language),
+        description = description_ABCD % {'type': 'A', 'language': language},
+        status = 2, # 1: draft, 2: published
+        max_instances = 1,
+        mentors = ['malavarena@gmail.com', 'rishig@zulipchat.com',
+                   'tabbott@zulipchat.com'],
+        tags = ['translation'], # free text
+        is_beginner = False,
+        # 1: Coding, 2: User Interface, 3: Documentation & Training,
+        # 4: Quality Assurance, 5: Outreach & Research
+        categories = [3, 5],
+        time_to_complete_in_days = 5, # must be between 3 and 7
+        # Field currently not accessible via API. gci-support says it is coming soon.
+        # external_url = "https://github.com/zulip/zulip-gci/blob/master/tasks/webhook-integrations.md",
+        private_metadata = "translations-A",
+        do_upload = args.force)
+
+    # Task Type D
+    upload_task(
+        # https://developers.google.com/open-source/gci/resources/downloads/TaskAPISpec.pdf
+        name = '%s: Improve style guide' % (language),
+        description = description_ABCD % {'type': 'D', 'language': language},
+        status = 2, # 1: draft, 2: published
+        max_instances = 1,
+        mentors = ['malavarena@gmail.com', 'rishig@zulipchat.com',
+                   'tabbott@zulipchat.com'],
+        tags = ['translation'], # free text
+        is_beginner = False,
+        # 1: Coding, 2: User Interface, 3: Documentation & Training,
+        # 4: Quality Assurance, 5: Outreach & Research
+        categories = [3],
+        time_to_complete_in_days = 5, # must be between 3 and 7
+        # Field currently not accessible via API. gci-support says it is coming soon.
+        # external_url = "https://github.com/zulip/zulip-gci/blob/master/tasks/webhook-integrations.md",
+        private_metadata = "translations-D",
+        do_upload = args.force)
+
+# Task Type E
+upload_task(
+    # https://developers.google.com/open-source/gci/resources/downloads/TaskAPISpec.pdf
+    name = 'Translations: Propose a language',
+    description = description_E,
+    status = 2, # 1: draft, 2: published
+    max_instances = 10,
+    mentors = ['malavarena@gmail.com', 'rishig@zulipchat.com',
+               'tabbott@zulipchat.com'],
+    tags = ['translation'], # free text
+    is_beginner = False,
+    # 1: Coding, 2: User Interface, 3: Documentation & Training,
+    # 4: Quality Assurance, 5: Outreach & Research
+    categories = [3],
+    time_to_complete_in_days = 5, # must be between 3 and 7
+    # Field currently not accessible via API. gci-support says it is coming soon.
+    # external_url = "https://github.com/zulip/zulip-gci/blob/master/tasks/webhook-integrations.md",
+    private_metadata = "translations-E",
+    do_upload = args.force)
+
+if not args.force:
+    print
+    print("No tasks uploaded. Add a -f argument to upload tasks to the GCI website.")
+    print("This is not idempotent. Running this twice with -f will create two sets of tasks.")
---
+++ tasks/translation-terms.md
@@ -0,0 +1,40 @@
+# Terms
+
+* Message
+* Private Message (PM)
+* Group PM
+* Realm
+* Stream
+* Topic
+* Private/Invite-Only Stream
+* Public Stream
+* Bot
+* Integration
+* Notification
+* Alert Word
+* View
+* Home
+* Emoji
+
+# Phrases
+
+* Subscribe to a stream/Unsubscribe from a stream
+* Narrow to
+* Filter
+* Mute/Unmute
+* Deactivate/Reactivate
+* Search
+* Pin
+* Mention/@mention
+* Invalid
+* Customization
+* I want
+* User
+* Person/People
+
+# Other
+
+* You
+* We
+
+& any Zulip-relevant terms in the *language* that should be specified
