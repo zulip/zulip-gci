dc3e379aa1ba5760c506b530e9f419d4c1f38a54
Rishi Gupta <rishig@zulipchat.com>
2017-03-17 12:47:08 -0700

    scripts: Add second QA task.

---
+++ scripts/quality-assurance-second.py
@@ -0,0 +1,38 @@
+import argparse
+from upload import upload_task
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--force', dest='force', action="store_true", default=False)
+args = parser.parse_args()
+
+description = """Find new bugs in Zulip!
+
+Instructions for quality assurance tasks are at
+https://github.com/zulip/zulip-gci/blob/master/tasks/quality-assurance.md.
+
+This task is for if you've already done the "Find bugs in Zulip" task, and
+found another bug!
+"""
+
+upload_task(
+    # https://developers.google.com/open-source/gci/resources/downloads/TaskAPISpec.pdf
+    name = 'Find your second bug in Zulip',
+    description = description,
+    status = 2, # 1: draft, 2: published
+    max_instances = 10,
+    mentors = ['rishig@zulipchat.com', 'tabbott@zulipchat.com'],
+    tags = ['bug squashing', 'internationalization', 'android', 'frontend'], # free text
+    is_beginner = False,
+    # 1: Coding, 2: User Interface, 3: Documentation & Training,
+    # 4: Quality Assurance, 5: Outreach & Research
+    categories = [2, 4],
+    time_to_complete_in_days = 3, # must be between 3 and 7
+    # Field currently not accessible via API. gci-support says it is coming soon.
+    # external_url = "https://github.com/zulip/zulip-gci/blob/master/tasks/quality-assurance.md",
+    private_metadata = "quality-assurance",
+    do_upload = args.force)
+
+if not args.force:
+    print
+    print("No tasks uploaded. Add a -f argument to upload tasks to the GCI website.")
+    print("This is not idempotent. Running this twice with -f will create two sets of tasks.")
