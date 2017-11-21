4a801c260e8d4683cddfb0722b24279e5a366289
derAnfaenger <robhoenig@gmail.com>
2017-11-21 17:23:50 +0100

    task scripts: Make upload_task params keyword-only.

--- scripts/2017/upload.py
+++ scripts/2017/upload.py
@@ -9,7 +9,7 @@ from task_uploader.client import GCIAPIClient
 # https://developers.google.com/open-source/gci/resources/downloads/TaskAPISpec.pdf
 # One field is missing in the documentation and in the argument list below: external_url.
 # gci-support says it is coming soon.
-def upload_task(name, description, status, max_instances, mentors, tags, is_beginner,
+def upload_task(*, name, description, status, max_instances, mentors, tags, is_beginner,
                 categories, time_to_complete_in_days, private_metadata, external_url, do_upload = False):
     task = locals()
     del task['do_upload']
