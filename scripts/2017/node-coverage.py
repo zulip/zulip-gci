612315cac82cfb31db46e1c1fa0d2a06ad8fc8ce
Joshua Pan <joshuapan8@gmail.com>
2017-11-27 19:19:59 -0800

    scripts: Update node-coverage script.

--- scripts/2017/node-coverage.py
+++ scripts/2017/node-coverage.py
@@ -6,15 +6,27 @@ parser.add_argument('-f', '--force', dest='force', action="store_true", default=
 args = parser.parse_args()
 
 files_uploaded = [
-    ('narrow_state.js', ['exports.operators', 'collect_single', 'exports.set_compose_defaults', 'exports.stream', 'exports.topic', 'exports.is_for_stream_id']),
-    ('stream_data.js',  ['exports.delete_sub', 'exports.invite_streams', 'exports.get_subscriber_count', 'exports.render_stream_description',
-                         'exports.canonicalized_name', 'exports.name_in_home_view', 'exports.notifications_in_home_view', 'exports.get_default_status',
-                         'exports.remove_subscriber', 'exports.user_is_subscribed', 'exports.create_sub_from_server_data', 'exports.receives_desktop_notifications',
-                         'exports.receives_audible_notifications', 'exports.initialize_from_page_params', 'exports.get_newbie_stream', 'exports.remove_default_stream']),
-    ('user_groups.js',  ['exports.get_user_group_from_id', 'exports.get_user_group_from_name', 'exports.initialize'])
+    ('narrow_state.js', ['exports.is_for_stream_id']),
+    ('stream_data.js',  ['exports.name_in_home_view', 'exports.notifications_in_home_view']),
+    ('user_groups.js',  ['exports.get_user_group_from_id'])
 ]
 
-files = []
+files = [
+    ('user_groups.js', ['exports.get_user_group_from_name']),
+    ('user_groups.js', ['exports.initialize']),
+    ('stream_data.js', ['exports.get_newbie_stream']),
+    ('stream_data.js', ['exports.initialize_from_page_params']),
+    ('stream_data.js', ['exports.receives_desktop_notifications', 'exports.receives_audible_notifications']),
+    ('stream_data.js', ['exports.delete_sub', 'exports.invite_streams']),
+    ('stream_data.js', ['exports.get_subscriber_count', 'exports.render_stream_description', 'exports.canonicalized_name']),
+    ('stream_data.js', ['exports.get_default_status']),
+    ('stream_data.js', ['exports.remove_subscriber', 'exports.user_is_subscribed']),
+    ('stream_data.js', ['exports.create_sub_from_server_data']),
+    ('stream_data.js', ['exports.remove_default_stream']),
+    ('narrow_state.js', ['exports.stream', 'exports.topic']),
+    ('narrow_state.js', ['exports.operators', 'collect_single']),
+    ('narrow_state.js', ['exports.set_compose_defaults'])
+]
 
 description = """Unit tests ensure the quality and correctness of your code, especially when doing
 large code refactorings, and prevent regressions. Zulip uses node to unit test its
